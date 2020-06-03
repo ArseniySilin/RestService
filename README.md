142f38130b9902bc05f623705b5807581a4843d9
Arseniy Silin <milessmiles@yandex.ru>
2020-06-03 22:20:31 +0300

    Add test

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 TODO:
 
-+ Unify User classes
 + Add tests
 + Refactor. Replace controllers, repos to separate folders
 + Add endpoint for link generation
---
+++ src/test/java/com/example/restservice/UsersRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.restservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRepositoryTest {
+
+  @Test
+  @DisplayName("Add user test")
+  void addUser() {
+  }
+}
