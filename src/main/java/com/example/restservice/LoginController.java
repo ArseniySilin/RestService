4b94b25c5635dd8f87d4e941894e2405a82945c6
Arseniy Silin <milessmiles@yandex.ru>
2020-06-03 22:52:21 +0300

    Add rollback User class

--- src/main/java/com/example/restservice/LoginController.java
+++ src/main/java/com/example/restservice/LoginController.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.core.userdetails.User;
 
 @RestController
 @CrossOrigin
