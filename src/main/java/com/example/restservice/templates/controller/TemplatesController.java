a46217ef7823e527607aba90745032ffb5527186
Arseniy Silin <milessmiles@yandex.ru>
2020-07-08 16:32:06 +0300

    Fix getAllWithFolders request params

--- src/main/java/com/example/restservice/templates/controller/TemplatesController.java
+++ src/main/java/com/example/restservice/templates/controller/TemplatesController.java
@@ -1,32 +1,27 @@
 package com.example.restservice.templates.controller;
 
 import com.example.restservice.CommonResponse;
+import com.example.restservice.Greeting;
 import com.example.restservice.Messages;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/templates")
 public class TemplatesController {
 
   @GetMapping(value = "/AllWithFolders")
-  public ResponseEntity<CommonResponse> getAllWithValues(
-    @RequestParam String folderKey,
-    @RequestParam(defaultValue = "1") String pageNumber,
-    @RequestParam(defaultValue = "15") String itemsPerPage,
-    @RequestParam(defaultValue = "2") String columnToOrderBy,
-    @RequestParam(defaultValue = "0") String orderBy
-  ) {
+  public ResponseEntity<CommonResponse> getAllWithFolders(@RequestParam Map<String, String> queryParams) {
+    String folderKey = queryParams.get("folderKey");
+    String pageNumber = queryParams.getOrDefault("pageNumber", "1");
+    String itemsPerPage = queryParams.getOrDefault("itemsPerPage", "15");
+    String columnToOrderBy = queryParams.getOrDefault("columnToOrderBy", "2");
+    String orderBy = queryParams.getOrDefault("orderBy", "0");
 
-    return ResponseEntity.ok(new CommonResponse(
-      Messages.SUCCESS.message,
-      Messages.SUCCESS.code,
-      null,
-      "data"
-    ));
+    return ResponseEntity.ok(new CommonResponse(Messages.SUCCESS.message, Messages.SUCCESS.code, null, ""));
   }
 
 }
